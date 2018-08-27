91035121889ea33047e53c7d5aae5e9b0792ed2f
John Xiong <imgk@mail.ustc.edu.cn>
2018-08-27 18:22:25 +0800

    replace BigInt with customed uint384

--- src/mac/Poly1305.jl
+++ src/mac/Poly1305.jl
@@ -1,59 +1,63 @@
 
 module Poly1305
 
+include("uint384.jl")
+
+using .uint384: UInt384, add, minus, multi, modulo
 using ..Common: @lebytes, @little!
 
 const Poly1305KeyLen = 32
-const prefix = BigInt(1) << 128
-const p = BigInt(1) << 130 - 5
+const p = minus(UInt384(0x00, 0x04, 0x00), 0x05)
 
-function Poly1305Cal(r::BigInt, a::BigInt, msg::Vector{UInt8}, isOver::Bool)::BigInt
+function Poly1305Cal(r::UInt384, a::UInt384, msg::Vector{UInt8}, isOver::Bool)::UInt384
     len = length(msg)
     nblock = fld(len, 16)
     msgBlock = unsafe_wrap(Array{UInt128}, Ptr{UInt128}(pointer(msg)), nblock)
 
-    a = foldl((x, y) -> (x + @little!(y) + prefix) * r % p, msgBlock; init=a)
+    t = UInt384(0x00, 0x01, 0x00)
+    a = foldl((x, y) -> begin t.l = @little!(y); modulo(multi(add(x, t, x), r, x), p, x) end, msgBlock; init=a)
 
     if (local left = len % 16; left != 0)
         block = zeros(UInt8, 16)
-        block[1:left] = unsafe_wrap(Array{UInt8}, pointer(msg)+nblock<<4, left)
+        @inbounds block[1:left] = unsafe_wrap(Array{UInt8}, pointer(msg)+nblock*16, left)
 
-        a = (a + 
-            if isOver
-                block[left+1] = 0x01
-                @little!(reinterpret(UInt128, block)[])
-            else
-                @little!(reinterpret(UInt128, block)[]) + prefix
-            end
-        ) * r % p
+        if isOver
+            block[left+1] = 0x01
+            t.m = 0x00
+            t.l = @little!(reinterpret(UInt128, block)[])
+        else
+            t.l = @little!(reinterpret(UInt128, block)[])
+        end
+
+        modulo(multi(add(a, t, a), r, a), p, a)
     end
 
-    return a
+    a
 end
 
 function Poly1305MAC(msg::Array{Vector{UInt8}}, key::Vector{UInt8})::Array{UInt8}
-    r, s = reinterpret(UInt128, key)
-    r = BigInt(@little!(r) & 0x0ffffffc0ffffffc0ffffffc0fffffff)
-    s = BigInt(@little!(s))
+    k = reinterpret(UInt128, key)
+    r = UInt384(0x00, 0x00, @little!(k[1]) & 0x0ffffffc0ffffffc0ffffffc0fffffff)
+    s = UInt384(0x00, 0x00, @little!(k[2]))
 
-    a = BigInt(0)
-    for i in 1:length(msg)-1
-        a = Poly1305Cal(r, a, msg[i], false)
+    a = UInt384(0x00)
+    @inbounds for i in 1:length(msg)-1
+        Poly1305Cal(r, a, msg[i], false)
     end
-    a = Poly1305Cal(r, a, msg[end], true) + s
+    add(Poly1305Cal(r, a, msg[end], true), s, a)
 
-    return @lebytes(a, 16)
+    @lebytes(a.l, 16)
 end
 
 function Poly1305MAC(msg::Vector{UInt8}, key::Vector{UInt8})::Array{UInt8}
-    r, s = reinterpret(UInt128, key)
-    r = BigInt(@little!(r) & 0x0ffffffc0ffffffc0ffffffc0fffffff)
-    s = BigInt(@little!(s))
+    k = reinterpret(UInt128, key)
+    r = UInt384(0x00, 0x00, @little!(k[1]) & 0x0ffffffc0ffffffc0ffffffc0fffffff)
+    s = UInt384(0x00, 0x00, @little!(k[2]))
 
-    a = BigInt(0)
-    a = Poly1305Cal(r, a, msg, true) + s
+    a = UInt384(0x00)
+    add(Poly1305Cal(r, a, msg, true), s, a)
 
-    return @lebytes(a, 16)
+    @lebytes(a.l, 16)
 end
 
 end # module
